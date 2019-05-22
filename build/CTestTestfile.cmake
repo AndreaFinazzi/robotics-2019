90dcf56549ab01d4ab2ab5f32d1a49897ddf995b
AndreaFinazzi <af.andreafinazzi@gmail.com>
2019-05-22 18:55:58 +0200

    First workspace commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/andreafinazzi/Documents/robotics-2019/src
+# Build directory: /home/andreafinazzi/Documents/robotics-2019/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
