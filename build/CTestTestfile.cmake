fc08478c309082adaf208a7a90427359ad86a275
AndreaFinazzi <af.andreafinazzi@gmail.com>
2019-05-23 15:31:07 +0200

    repo now contains just the odometry package.

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andreafinazzi/Documents/robotics-2019/src
-# Build directory: /home/andreafinazzi/Documents/robotics-2019/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
