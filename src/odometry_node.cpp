590cf1b687d6a741f724061f1db13627b661546f
AndreaFinazzi <af.andreafinazzi@gmail.com>
2019-05-24 12:05:08 +0200

    output data fixed with odometry data. Car measurments fixed

--- src/odometry_node.cpp
+++ src/odometry_node.cpp
@@ -19,8 +19,8 @@
 //#include "custom_messages/floatStamped.h"
 #include "odometry/floatStamped.h"
 
-#define BASELINE 0.130
-#define CAR_LENGTH 0.1765
+#define BASELINE 1.30
+#define CAR_LENGTH 1.765
 #define STEERING_FACTOR 18
 #define PI 3.14159265
 
@@ -28,11 +28,18 @@
 #define SPEED_R_TOPIC "speedR_stamped"
 #define STEER_TOPIC "steer_stamped"
 
+#define ODOMETRY_TOPIC_ID "odometry"
+#define ODOMETRY_FRAME_ID "odometry"
+#define CHILD_FRAME_ID "base_link"
 
-typedef struct odometry_data{
+typedef struct last_odometry_data{
     double x;
     double y;
     double theta;
+
+    double linear_velocity_x;
+    double linear_velocity_y;
+    double angular_velocity;
 }  OdometryData;
 
 typedef struct diff_drive_control_variables{
@@ -42,7 +49,7 @@ typedef struct diff_drive_control_variables{
 
 
 using namespace std;
-OdometryData odom;
+OdometryData last_odometry_data;
 double last_msg_time;
 
 void configChangeCallback(odometry::parametersConfig& config, uint32_t level) {
@@ -50,51 +57,58 @@ void configChangeCallback(odometry::parametersConfig& config, uint32_t level) {
             config.odometry_model_mode?"True":"False");
 }
 
-
-
-
-void differenrialDriveOdometry(double dt, double speed_L, double speed_R, OdometryData startingPose, OdometryData* storageStruct){
+void differenrialDriveOdometry(double delta_time, double speed_L, double speed_R, OdometryData& new_odometry_data){
+    new_odometry_data.angular_velocity = (speed_R - speed_L) / BASELINE;
+    
     double linear_velocity = (speed_R + speed_L) / 2;
-    double angular_velocity = (speed_R - speed_L) / BASELINE;
-    storageStruct->theta = startingPose.theta + angular_velocity * dt;
-    if (storageStruct->theta > 2*PI)
-    {
-        storageStruct->theta -= 2*PI;
-    }else if (storageStruct->theta < 0){
-        storageStruct->theta += 2*PI;
+    
+    double delta_theta = new_odometry_data.angular_velocity * delta_time;
+    new_odometry_data.theta = last_odometry_data.theta + delta_theta;
+    
+    if (new_odometry_data.theta > 2*PI) {
+
+        new_odometry_data.theta -= 2*PI;
+
+    } else if (new_odometry_data.theta < 0) {
+
+        new_odometry_data.theta += 2*PI;
+
     }
     
     /*  if (angular_velocity > 0.0001)  //exact integration assuming velocities constamìnt in the time lapse
     {
-        storageStruct->x = startingPose.x + (linear_velocity / angular_velocity) * (sin(storageStruct->theta * (180/PI)) - sin(startingPose.theta * (180/PI)));
-        storageStruct->y = startingPose.y - (linear_velocity / angular_velocity) * (cos(storageStruct->theta * (180/PI)) - sin(startingPose.theta * (180/PI)));
+        new_odometry_data->x = startingPose.x + (linear_velocity / angular_velocity) * (sin(new_odometry_data->theta * (180/PI)) - sin(startingPose.theta * (180/PI)));
+        new_odometry_data->y = startingPose.y - (linear_velocity / angular_velocity) * (cos(new_odometry_data->theta * (180/PI)) - sin(startingPose.theta * (180/PI)));
     }*/
- //   else  //for low values of omega I use the Ruge-Kutta approximation
- //   {
-        storageStruct->x = startingPose.x + linear_velocity * dt * cos(startingPose.theta + (angular_velocity * dt) / 2);
-        storageStruct->y = startingPose.y + linear_velocity * dt * sin(startingPose.theta + (angular_velocity * dt) / 2);
+//   else  //for low values of omega I use the Ruge-Kutta approximation
+//   {
+     new_odometry_data.linear_velocity_x = linear_velocity * cos(last_odometry_data.theta + (delta_theta / 2));
+     new_odometry_data.linear_velocity_y = linear_velocity * sin(last_odometry_data.theta + (delta_theta / 2));
+     new_odometry_data.x = last_odometry_data.x + (new_odometry_data.linear_velocity_x * delta_time);
+     new_odometry_data.y = last_odometry_data.y + (new_odometry_data.linear_velocity_y * delta_time);
 //    }
 }
 
-void ackermanDriveOdometry(double dt, double speed_L, double speed_R, double steer, OdometryData startingPose, OdometryData* storageStruct){
+void ackermanDriveOdometry(double delta_time, double speed_L, double speed_R, double steer, OdometryData startingPose, OdometryData* new_odometry_data){
     double linear_velocity = (speed_R + speed_L) / 2;
     double angular_velocity = linear_velocity * tan(steer / STEERING_FACTOR) / CAR_LENGTH;
-    storageStruct->theta = startingPose.theta + angular_velocity * dt;
-    if (storageStruct->theta > 2*PI)
+    double delta_theta = angular_velocity * delta_time;
+    new_odometry_data->theta = startingPose.theta + delta_theta;
+    if (new_odometry_data->theta > 2*PI)
     {
-        storageStruct->theta -= 2*PI;
-    }else if (storageStruct->theta < 0){
-        storageStruct->theta += 2*PI;
+        new_odometry_data->theta -= 2*PI;
+    }else if (new_odometry_data->theta < 0){
+        new_odometry_data->theta += 2*PI;
     }
-  /*  if (angular_velocity > 0.0001)  //exact integration assuming velocities constamìnt in the time lapse
+ /*  if (angular_velocity > 0.0001)  //exact integration assuming velocities constamìnt in the time lapse
     {
-        storageStruct->x = startingPose.x + (linear_velocity / angular_velocity) * (sin(storageStruct->theta * (180/PI)) - sin(startingPose.theta * (180/PI)));
-        storageStruct->y = startingPose.y - (linear_velocity / angular_velocity) * (cos(storageStruct->theta * (180/PI)) - sin(startingPose.theta * (180/PI)));
+        new_odometry_data->x = startingPose.x + (linear_velocity / angular_velocity) * (sin(new_odometry_data->theta * (180/PI)) - sin(startingPose.theta * (180/PI)));
+        new_odometry_data->y = startingPose.y - (linear_velocity / angular_velocity) * (cos(new_odometry_data->theta * (180/PI)) - sin(startingPose.theta * (180/PI)));
     }*/
- //   else  //for low values of omega I use the Ruge-Kutta approximation
- //   {
-        storageStruct->x = startingPose.x + linear_velocity * dt * cos(startingPose.theta + (angular_velocity * dt) / 2);
-        storageStruct->y = startingPose.y + linear_velocity * dt * sin(startingPose.theta + (angular_velocity * dt) / 2);
+//   else  //for low values of omega I use the Ruge-Kutta approximation
+//   {
+        new_odometry_data->x = startingPose.x + linear_velocity * delta_time * cos(startingPose.theta + (delta_theta / 2));
+        new_odometry_data->y = startingPose.y + linear_velocity * delta_time * sin(startingPose.theta + (delta_theta / 2));
 //    }
 }
 
@@ -102,20 +116,20 @@ nav_msgs::Odometry populateOdometry() {
     
     nav_msgs::Odometry odometry;
     odometry.header.stamp = ros::Time::now();
-    odometry.header.frame_id = "odometry";
+    odometry.header.frame_id = ODOMETRY_FRAME_ID;
 
     //TODO: these should be set with calculated values
     // position
-    odometry.pose.pose.position.x = 1;
-    odometry.pose.pose.position.y = 2;
+    odometry.pose.pose.position.x = last_odometry_data.x;
+    odometry.pose.pose.position.y = last_odometry_data.y;
     odometry.pose.pose.position.z = 0.0;
-    odometry.pose.pose.orientation = tf::createQuaternionMsgFromYaw(123.4);
+    odometry.pose.pose.orientation = tf::createQuaternionMsgFromYaw(last_odometry_data.theta);
 
     //set velocity
-    odometry.child_frame_id = "base_link";
-    odometry.twist.twist.linear.x = 0.5;
-    odometry.twist.twist.linear.y = 0.5;
-    odometry.twist.twist.angular.z = 123.4;
+    odometry.child_frame_id = CHILD_FRAME_ID;
+    odometry.twist.twist.linear.x = last_odometry_data.linear_velocity_x;
+    odometry.twist.twist.linear.y = last_odometry_data.linear_velocity_y;
+    odometry.twist.twist.angular.z = last_odometry_data.angular_velocity;
 
     return odometry;
 }
@@ -123,14 +137,14 @@ nav_msgs::Odometry populateOdometry() {
 geometry_msgs::TransformStamped populateTransform() {
     geometry_msgs::TransformStamped odometry_transform;
     odometry_transform.header.stamp = ros::Time::now();
-    odometry_transform.header.frame_id = "odometry";
-    odometry_transform.child_frame_id = "base_link";
+    odometry_transform.header.frame_id = ODOMETRY_FRAME_ID;
+    odometry_transform.child_frame_id = CHILD_FRAME_ID;
 
     //TODO: these should be set with calculated values
-    odometry_transform.transform.translation.x = 1;
-    odometry_transform.transform.translation.y = 2;
+    odometry_transform.transform.translation.x = last_odometry_data.x;
+    odometry_transform.transform.translation.y = last_odometry_data.y;
     odometry_transform.transform.translation.z = 0.0;
-    odometry_transform.transform.rotation = tf::createQuaternionMsgFromYaw(123.4);
+    odometry_transform.transform.rotation = tf::createQuaternionMsgFromYaw(last_odometry_data.theta);
 
     return odometry_transform;
 }
@@ -140,17 +154,21 @@ void subCallback(const odometry::floatStamped::ConstPtr& left,
                  const odometry::floatStamped::ConstPtr& steer,
                  tf::TransformBroadcaster& broadcaster,
                  ros::Publisher& publisher){
-    double time = (left->header.stamp.toSec() + right->header.stamp.toSec() + steer->header.stamp.toSec()) / 3;
-    double dt = time - last_msg_time;
-    last_msg_time = time;
-    OdometryData storageStruct;
-    differenrialDriveOdometry(dt, left->data, right->data, odom, &storageStruct);
-    double deltax = storageStruct.x-odom.x;
-    double deltay = storageStruct.y-odom.y;
-    double speed = (right->data + left->data) / 2; 
-    odom = storageStruct;
 
-    ROS_INFO("[\nX COORDINATE: %f\nY COORDINATE: %f\nORIENTATION: %f\ndt: %f\nSPEED: %f\nDELTA: %f   %f", odom.x, odom.y, odom.theta * 360/ (2*PI), dt, speed, deltax, deltay);
+    double time = (left->header.stamp.toSec() + right->header.stamp.toSec() + steer->header.stamp.toSec()) / 3;
+    double delta_time = time - last_msg_time;
+    last_msg_time = time;
+
+    OdometryData new_odometry_data;
+    differenrialDriveOdometry(delta_time, left->data, right->data, new_odometry_data);
+
+    double deltax = new_odometry_data.x - last_odometry_data.x;
+    double deltay = new_odometry_data.y - last_odometry_data.y;
+    double speed = (right->data + left->data) / 2;
+
+    last_odometry_data = new_odometry_data;
+
+    ROS_INFO("[\nX COORDINATE: %f\nY COORDINATE: %f\nORIENTATION: %f\ndelta_time: %f\nSPEED: %f\nDELTA: %f   %f", last_odometry_data.x, last_odometry_data.y, last_odometry_data.theta * 360/ (2*PI), delta_time, speed, deltax, deltay);
     
     // publishing and broadcasting
     nav_msgs::Odometry odometry = populateOdometry();
@@ -163,13 +181,6 @@ void subCallback(const odometry::floatStamped::ConstPtr& left,
 
 int main(int argc, char *argv[])
 {
-
-    //Odometry config variables
-    OdometryData odometry;
-    odometry.x = 0;
-    odometry.y = 0;
-    odometry.theta = 0;
-    double last_msg_time = 0.0;
 
     ros::init(argc, argv, "odometry_node");
     ros::NodeHandle nh;
